3e82efc5de8151ce9dd95a619b8b65afa7aa407d
Nabo <110341514+Navomeister@users.noreply.github.com>
2023-04-18 14:48:04 -0300

    Add files via upload

--- lib/frasesMassa.dart
+++ lib/frasesMassa.dart
@@ -69,14 +69,14 @@ class _FrasesState extends State<Frases> {
     atual = aleatorio;
     while (atual == aleatorio) {
       setState(() {
-        aleatorio = Random().nextInt(10);
+        aleatorio = Random().nextInt(lista.length);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    
+
     // front end do aplicativo
     return Scaffold(
       appBar: AppBar(
@@ -91,8 +91,10 @@ class _FrasesState extends State<Frases> {
       ),
 
       // corpo central do app
+      // Stack para poder colocar elementos um em cima do outro
       body: Stack(
         children: <Widget>[
+          // Container contendo a imagem de fundo
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
@@ -102,9 +104,13 @@ class _FrasesState extends State<Frases> {
               ),
             ),
           ),
+
+          // Coluna centralizada(?) com os elementos em cima da imagem de fundo
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+
+              // Título do corpo centralizado
               Center(
                 child: Padding(
                   padding: EdgeInsets.only(top: 20),
@@ -114,6 +120,8 @@ class _FrasesState extends State<Frases> {
                   ),
                 ),
               ),
+
+              // Imagem da Frase centralizada
               Center(
                 child: Padding(
                   padding: EdgeInsets.only(top: 30),
@@ -125,6 +133,8 @@ class _FrasesState extends State<Frases> {
                   ),
                 ),
               ),
+
+              // Frase centralizada, com tamanho fixo para evitar que o botão se desloque
               Center(
                 child: Padding(
                     padding: EdgeInsets.only(top: 30),
@@ -137,6 +147,8 @@ class _FrasesState extends State<Frases> {
                       ),
                     )),
               ),
+
+              // Botão com padding para dar espaço da Frase
               Padding(
                   padding: EdgeInsets.only(top: 20),
                   child: SizedBox(
